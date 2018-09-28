9ac976cf31fede982cfc377449ece0699487947f
Roman Bezpalov <roman.bezpalov96@gmail.com>
2018-09-28 15:41:22 +0300

    addApp

---
+++ SunriseSunset/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SunriseSunset
+//
+//  Created by Roman Bezpalov on 28.09.18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
